057ebc21eeb391ec3b1efa223acb51b40448db8e
askming <askming@gmail.com>
2014-10-22 11:22:48 -0500

    init

---
+++ 1. QRJM/simulaiton/R code/simulation1.R
@@ -0,0 +1,12 @@
+##### R code to do simulation study of longitudianl quantile regression with informative drop-outs using JM approach ####
+
+#### date: 2014/10/22 (happy birthday to Lan!)####
+#### author: ming yang ####
+
+
+### 1. simulate data ###
+
+
+
+
+### 2. run the model ###
